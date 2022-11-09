adbc29f43b08d96ebdb74e1c09c44157433d84aa
Sam V <sam.vanheer@outlook.com>
2022-11-09 23:46:30 +0100

    Remove unused Version constant

--- HalfLife.UnifiedSdk.MapDecompiler.GUI/MapDecompilerConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HalfLife.UnifiedSdk.MapDecompiler.GUI
-{
-    internal static class MapDecompilerConstants
-    {
-        // TODO: can probably extract the version number embedded in the assembly.
-        public const string Version = "1.0";
-    }
-}
