7ab8b04db1ddddcdf24c6414c53f4973e71d04f0
Sam V <sam.vanheer@outlook.com>
2022-11-10 18:51:49 +0100

    Rework log output controls to use AvaloniaEdit TextArea

---
+++ HalfLife.UnifiedSdk.MapDecompiler.GUI/Behaviors/DocumentTextBindingBehavior.cs
@@ -0,0 +1,62 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.LogicalTree;
+using Avalonia.Xaml.Interactivity;
+using AvaloniaEdit.Editing;
+using System;
+
+namespace HalfLife.UnifiedSdk.MapDecompiler.GUI.Behaviors
+{
+    public class DocumentTextBindingBehavior : Behavior<TextArea>
+    {
+        private TextArea? _textArea;
+        private ScrollViewer? _scrollViewer;
+
+        private bool _needToScrollToEnd;
+
+        public static readonly StyledProperty<string?> TextProperty =
+            AvaloniaProperty.Register<DocumentTextBindingBehavior, string?>(nameof(Text));
+
+        public string? Text
+        {
+            get => GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            if (AssociatedObject is TextArea textArea)
+            {
+                _textArea = textArea;
+                _scrollViewer = _textArea.GetLogicalParent<ScrollViewer?>();
+                this.GetObservable(TextProperty).Subscribe(TextPropertyChanged);
+
+                if (_scrollViewer is not null)
+                {
+                    _scrollViewer.ScrollChanged += ScrollViewer_ScrollChanged;
+                }
+            }
+        }
+
+        private void TextPropertyChanged(string? text)
+        {
+            if (_textArea is not null)
+            {
+                _textArea.Document ??= new();
+                _textArea.Document.Text = text ?? string.Empty;
+                _needToScrollToEnd = true;
+            }
+        }
+
+        private void ScrollViewer_ScrollChanged(object? sender, ScrollChangedEventArgs e)
+        {
+            if (_needToScrollToEnd)
+            {
+                _needToScrollToEnd = false;
+                _scrollViewer!.ScrollToEnd();
+            }
+        }
+    }
+}
--- HalfLife.UnifiedSdk.MapDecompiler.GUI/Views/DecompilerOutputView.axaml.cs
+++ HalfLife.UnifiedSdk.MapDecompiler.GUI/Views/DecompilerOutputView.axaml.cs
@@ -4,48 +4,11 @@ namespace HalfLife.UnifiedSdk.MapDecompiler.GUI.Views
 {
     public partial class DecompilerOutputView : UserControl
     {
-        private bool _programAutoScroll = true;
-        private bool _jobAutoScroll = true;
-
         public DecompilerOutputView()
         {
             InitializeComponent();
-        }
-
-        private void ProgramScrollViewer_ScrollChanged(object sender, ScrollChangedEventArgs e)
-        {
-            ScrollViewerScrollChanged(ProgramScrollViewer, e, ref _programAutoScroll);
-        }
-
-        private void JobScrollViewer_ScrollChanged(object sender, ScrollChangedEventArgs e)
-        {
-            ScrollViewerScrollChanged(JobScrollViewer, e, ref _jobAutoScroll);
-        }
-
-        // Based on https://stackoverflow.com/a/19315242/1306648
-        private static void ScrollViewerScrollChanged(ScrollViewer sv, ScrollChangedEventArgs e, ref bool autoScroll)
-        {
-            // User scroll event : set or unset auto-scroll mode
-            if (e.ExtentDelta.Length == 0)
-            {   // Content unchanged : user scroll event
-                if (sv.Offset.Y == (sv.Extent.Height - sv.Viewport.Height))
-                {   // Scroll bar is in bottom
-                    // Set auto-scroll mode
-                    autoScroll = true;
-                }
-                else
-                {   // Scroll bar isn't in bottom
-                    // Unset auto-scroll mode
-                    autoScroll = false;
-                }
-            }
-
-            // Content scroll event : auto-scroll eventually
-            if (autoScroll && e.ExtentDelta.Length != 0)
-            {   // Content changed and auto-scroll mode set
-                // Autoscroll
-                sv.ScrollToEnd();
-            }
+            ProgramTextArea.Options.AllowScrollBelowDocument = false;
+            JobTextArea.Options.AllowScrollBelowDocument = false;
         }
     }
 }
